979446c06a9ac2fc9dbfac4a9ef0df720f81bf8e
Mateusz Sopiński <mateusz.sopinski@mirumee.com>
2023-08-17 18:14:40 +0200

    Add cache backend which uses DynamoDB

---
+++ ariadne_graphql_proxy/contrib/aws/cache_backend.py
@@ -0,0 +1,66 @@
+import time
+from typing import Any, Optional
+
+from boto3.dynamodb.conditions import Attr, Key
+from boto3.session import Session
+
+from ariadne_graphql_proxy.cache import CacheBackend
+
+
+class DynamoDBCacheError(Exception):
+    """Exception thrown by DynamoDBCacheBackend."""
+
+
+class DynamoDBCacheBackend(CacheBackend):
+    def __init__(
+        self,
+        table_name: str,
+        partition_key: str = "key",
+        ttl_attribute: str = "ttl",
+        session: Optional[Session] = None,
+    ) -> None:
+        self.table_name = table_name
+        self.partition_key = partition_key
+        self.ttl_attribute = ttl_attribute
+        self.value_attribute_name = "value"
+
+        self.session = session if session else Session()
+        self.dynamodb_resource = self.session.resource("dynamodb")
+        self.table = self.dynamodb_resource.Table(self.table_name)
+
+        self._assert_table_exists()
+
+    def _assert_table_exists(self):
+        not_found_exception = (
+            self.dynamodb_resource.meta.client.exceptions.ResourceNotFoundException
+        )
+        try:
+            self.table.table_status
+        except not_found_exception as exc:
+            raise DynamoDBCacheError(
+                f'Unable to access "{self.table_name}" table.'
+            ) from exc
+
+    async def set(self, key: str, value: Any, ttl: Optional[int] = None):
+        item = {self.partition_key: key, self.value_attribute_name: value}
+        if ttl:
+            now = int(time.time())
+            item[self.ttl_attribute] = now + ttl
+
+        self.table.put_item(Item=item)
+
+    async def get(self, key: str, default: Any = None) -> Any:
+        response = self.table.query(
+            KeyConditionExpression=Key(self.partition_key).eq(key),
+            FilterExpression=Attr(self.ttl_attribute).gte(int(time.time()))
+            | Attr(self.ttl_attribute).not_exists(),
+        )
+
+        items = response.get("Items", [])
+        if len(items) < 1:
+            return default
+
+        return items[0].get(self.value_attribute_name, default)
+
+    async def clear_all(self):
+        pass

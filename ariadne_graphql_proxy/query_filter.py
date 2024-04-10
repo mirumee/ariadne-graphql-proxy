bee71e8215b44824a08a8f9dd41137e904e168ff
Damian Czajkowski <d0.czajkowski@gmail.com>
2024-04-10 12:54:34 +0200

    fixed-variables-process-to-handle-objects-and-list-variables

--- ariadne_graphql_proxy/query_filter.py
+++ ariadne_graphql_proxy/query_filter.py
@@ -7,7 +7,9 @@ from graphql import (
     FragmentSpreadNode,
     GraphQLSchema,
     InlineFragmentNode,
+    ListValueNode,
     NameNode,
+    ObjectValueNode,
     OperationDefinitionNode,
     SelectionNode,
     SelectionSetNode,
@@ -147,18 +149,33 @@ class QueryFilter:
             ),
         )
 
+    def extract_variables(
+        self,
+        value: VariableNode | ListValueNode | ObjectValueNode,
+        context: QueryFilterContext,
+    ):
+        if isinstance(value, VariableNode):
+            context.variables.add(value.name.value)
+        elif isinstance(value, ObjectValueNode):
+            for field in value.fields:
+                self.extract_variables(field.value, context)  # type: ignore
+        elif isinstance(value, ListValueNode):
+            for item in value.values:
+                self.extract_variables(item, context)  # type: ignore
+
+    def update_context_variables(
+        self, field_node: FieldNode, context: QueryFilterContext
+    ):
+        for argument in field_node.arguments:
+            self.extract_variables(argument.value, context)  # type: ignore
+
     def filter_field_node(
         self,
         field_node: FieldNode,
         schema_obj: str,
         context: QueryFilterContext,
     ) -> Optional[FieldNode]:
-        context.variables.update(
-            argument.value.name.value
-            for argument in field_node.arguments
-            if isinstance(argument.value, VariableNode)
-        )
-
+        self.update_context_variables(field_node, context)
         if not field_node.selection_set:
             return field_node
 
